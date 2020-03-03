99783d1878634f5075fc0fed965a2df059b6a530
David Lynam <davel@a-w-s.co.uk>
2020-03-03 09:20:51 +0000

    Page 28 Tutorial Sheet 1 - Index not working, check naming

---
+++ Filmster/Models/DBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Filmster.Models
+{
+    public class DBContext : DbContext
+    {
+        public DbSet<Film> Films { get; set; }
+    }
+}
---
+++ Filmster/Models/Film.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Filmster.Models
+{
+    [Table("FILM")]
+    public class Film
+    {
+        [Column("film_id")]
+        public int FilmID { get; set; }
+        [Column("genre_id")]
+        public int GenreID { get; set; }
+        [Column("certificate_id")]
+        public int CertificateID { get; set; }
+        [Column("image_Id")]
+        public int ImageID { get; set; }
+        [Column("title")]
+        public string Title { get; set; }
+        [Column("synopsis")]
+        public string Synopsis { get; set; }
+        [Column("runtime")]
+        public int Runtime { get; set; }
+        [Column("release_date")]
+        public DateTime ReleaseDate { get; set; }
+
+
+        public string FilmDescTrimmed;
+    }
+}
