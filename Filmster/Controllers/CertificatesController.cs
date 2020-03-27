7959ed8b1390b8a2f42bf38b3f7195238a9de741
David Lynam <davel@a-w-s.co.uk>
2020-03-27 23:13:47 +0000

    All entities appear to have CRUD. Double check this, then focus on interaction links between pages, searching and pagination.

---
+++ Filmster/Controllers/CertificatesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Filmster.Models;
+
+namespace Filmster.Controllers
+{
+    public class CertificatesController : Controller
+    {
+        private DBContext db = new DBContext();
+
+        // GET: Certificates
+        public ActionResult Index()
+        {
+            return View(db.Certificates.ToList());
+        }
+
+        // GET: Certificates/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Certificate certificate = db.Certificates.Find(id);
+            if (certificate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(certificate);
+        }
+
+        // GET: Certificates/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Certificates/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CertificateId,CertificateName")] Certificate certificate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Certificates.Add(certificate);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(certificate);
+        }
+
+        // GET: Certificates/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Certificate certificate = db.Certificates.Find(id);
+            if (certificate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(certificate);
+        }
+
+        // POST: Certificates/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CertificateId,CertificateName")] Certificate certificate)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(certificate).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(certificate);
+        }
+
+        // GET: Certificates/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Certificate certificate = db.Certificates.Find(id);
+            if (certificate == null)
+            {
+                return HttpNotFound();
+            }
+            return View(certificate);
+        }
+
+        // POST: Certificates/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Certificate certificate = db.Certificates.Find(id);
+
+            List<Film> films = db.Films.Where(x => x.CertificateId == id).ToList();
+
+            if (films.Count > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                db.Certificates.Remove(certificate);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
